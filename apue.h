96250bc8ff9233fb74ba1e597f0892065bff83b4
Sukesan1984 <takamikousuke@gmail.com>
2021-01-16 18:53:49 +0900

    Add show process id

---
+++ apue.h
@@ -0,0 +1,18 @@
+#ifndef _APUE_H
+# define _APUE_H
+
+
+# define MAXLINE 4096
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/termios.h>
+#include <sys/ioctl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+#endif
