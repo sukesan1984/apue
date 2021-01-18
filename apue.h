514c5c9b4401305e3d27ef75b6a14fbb766ae202
Sukesan1984 <takamikousuke@gmail.com>
2021-01-18 17:45:57 +0900

    Add 3.1/3.2

--- apue.h
+++ apue.h
@@ -3,6 +3,8 @@
 
 
 # define MAXLINE 4096
+# define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+# define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/termios.h>
@@ -14,6 +16,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+
 
 void err_ret(const char *, ...);
 void err_sys(const char *, ...) __attribute__((noreturn));
